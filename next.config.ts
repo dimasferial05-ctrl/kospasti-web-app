3c0172d9e33b63428570b22fe5ae4c23379d1165
Dimasefha <dimasferial05@gmail.com>
2026-09-14 10:47:25 +0700

    Fix SQLite Database File Tracing for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Memberi tahu Vercel untuk membawa file database SQLite saat deploy
+  experimental: {
+    outputFileTracingIncludes: {
+      "/**": ["./prisma/**/*"],
+    },
+  },
 };
 
 export default nextConfig;
